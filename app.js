89585d056e56019f0088d2315901e86ce08e7781
Josh Mather <joshmather@gmail.com>
2021-11-09 10:42:47 -0800

    SA Updates to fork

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 require('dotenv').config();
-const stripe = require('stripe');
+const stripe = require('stripe')('sk_test_51JtI7yErofK4XECd5tQ582VKPNAKHirdexiIh03kkpPefHIuJnr1Z8TC25qKns1e9g2lmSrlStvejWXZsH0A1KfI00dMVkrw4v');
 
 var app = express();
 
@@ -11,13 +11,14 @@ app.engine('hbs', exphbs({
   defaultLayout: 'main',
   extname: '.hbs'
 }));
+
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json({}));
 
 /**
- * Home route
+ * Home route - this will render the sample store
  */
 app.get('/', function(req, res) {
   res.render('index');
@@ -25,43 +26,81 @@ app.get('/', function(req, res) {
 
 /**
  * Checkout route
+ * This route will render the checkout page with payment funcationality
+ * based on which item was chosen from the store
  */
-app.get('/checkout', function(req, res) {
-  // Just hardcoding amounts here to avoid using a database
+
+app.get('/checkout', async (req, res) => {
   const item = req.query.item;
   let title, amount, error;
+
 
   switch (item) {
     case '1':
       title = "The Art of Doing Science and Engineering"
-      amount = 2300      
+      amount = 2300
       break;
     case '2':
       title = "The Making of Prince of Persia: Journals 1985-1993"
       amount = 2500
-      break;     
+      break;
     case '3':
       title = "Working in Public: The Making and Maintenance of Open Source"
-      amount = 2800  
-      break;     
+      amount = 2800
+      break;
     default:
       // Included in layout view, feel free to assign error
-      error = "No item selected"      
+      error = "No item selected"
       break;
   }
 
-  res.render('checkout', {
-    title: title,
+  /*
+  * Create a PaymentIntent object to represent your intent
+  * to collect payment from the customer.
+  */
+  const intent = await stripe.paymentIntents.create({
     amount: amount,
-    error: error
+    currency: 'USD',
+    payment_method_types: [
+      'card',
+    ],
   });
+
+  //We will use the payment intent client secret to confirm payment when the consumer makes a purchase
+  res.render('checkout', { client_secret: intent.client_secret, amount: amount, title: title });
 });
 
 /**
  * Success route
  */
-app.get('/success', function(req, res) {
-  res.render('success');
+app.get('/success', async (req, res) => {
+  const payment_intent_id = req.query.payment_intent;
+  let message;
+
+  //Retrieve the payment intent from the confirm payment call on the client side
+  const intent = await stripe.paymentIntents.retrieve(payment_intent_id);
+
+  switch (intent.status) {
+    case 'succeeded':
+      message = "Success! Payment Recieved."
+      break;
+
+    case 'processing':
+      message = "Payment processing. We'll update you when payment is received."
+      break;
+
+    case 'requires_payment_method':
+      message = 'Payment failed. Please try another payment method.';
+      // Redirect your user back to your payment page to attempt collecting
+      // payment again
+      break;
+
+    default:
+      message = 'Something went wrong.';
+      break;
+  }
+
+  res.render('success', {message: message, intent_id: intent.id, amount: intent.amount});
 });
 
 /**
---
+++ public/js/checkout.js
@@ -0,0 +1,64 @@
+const stripe = Stripe("pk_test_51JtI7yErofK4XECd2dYbSI2lxpvJ8EB23nbt6QepueEjbnedAIq22ddhaWYF6tpgNjVKKIXX1tWYU7XYQXEIEjh500te4KedYW");
+
+let elements;
+
+initialize();
+
+document
+  .querySelector("#payment-form")
+  .addEventListener("submit", handleSubmit);
+
+/**
+ * Initialize the payment elements for payment collection
+ */
+async function initialize() {
+  elements = stripe.elements({clientSecret: secret});
+  const paymentElement = elements.create("payment");
+  paymentElement.mount("#payment-element");
+}
+
+/**
+ * Handle the payment submition.
+ * This will call the confirmPayment endpoint with 
+ * the collected payment details and provide the URL
+ * to be called back to when the payment attempt is complete
+ */
+async function handleSubmit(e) {
+  e.preventDefault();
+
+  setLoading(true);
+
+  const { error } = await stripe.confirmPayment({
+    elements,
+    confirmParams: {
+      return_url: "http://localhost:3000/success",
+    },
+  });
+
+
+  if (error) {
+    // This point will only be reached if there is an immediate error when
+    // confirming the payment. Show error to your customer (e.g., payment
+    // details incomplete)
+    const messageContainer = document.querySelector('#error-message');
+    messageContainer.textContent = error.message;
+  } else {
+    // Your customer will be redirected to your `return_url`. For some payment
+    // methods like iDEAL, your customer will be redirected to an intermediate
+    // site first to authorize the payment, then redirected to the `return_url`.
+  }
+
+  setLoading(false);
+}
+
+// Show a spinner on payment submission
+function setLoading(isLoading) {
+  if (isLoading) {
+    // Disable the button and show a spinner
+    document.querySelector("#submit").disabled = true;
+    document.querySelector("#spinner").classList.remove("hidden");
+  } else {
+    document.querySelector("#submit").disabled = false;
+    document.querySelector("#spinner").classList.add("hidden");
+  }
+}
